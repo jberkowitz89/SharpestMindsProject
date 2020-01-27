e550d89a363db14754bd77ba1133826db1ce2f5f
Kevin <kjchoi10@gmail.com>
2020-01-27 12:08:59 -0500

    kevin update

---
+++ Nintendo_Project/Nintendo/src/main.py
@@ -0,0 +1,9 @@
+import os
+import sys
+
+sys.path.append(os.getcwd())
+
+import Nintendo.src.NintendoLife_Scraping as Nintendo
+
+link3710_pages = Nintendo.get_max_pages("link3710")
+link3710_ratings = Nintendo.games_ratings_to_df("link3710", link3710_pages)
