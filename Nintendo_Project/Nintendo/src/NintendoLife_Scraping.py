b8804aa56e6a15d0572dcef91232f57b6a127cfe
jberkowitz89 <jberkowitz89@gmail.com>
2020-01-29 23:02:45 -0500

    updating

--- Nintendo_Project/Nintendo/src/NintendoLife_Scraping.py
+++ Nintendo_Project/Nintendo/src/NintendoLife_Scraping.py
@@ -35,7 +35,7 @@ def get_max_pages(user_name):
         max_page_tag = 1
         return max_page_tag
     
-#Creating Function for getting games and ratings into dataframe=
+#Creating Function for getting games and ratings into dataframe
 def games_ratings_to_df(user_name, max_pages):
     """
     :params user_name: user_name
@@ -110,3 +110,66 @@ def games_ratings_to_df(user_name, max_pages):
     df["User"] = user_name
     
     return df
+
+#Function for getting the max number of pages for a thread
+def get_max_pages_thread(thread_name):
+    """
+    :params thread_name: thread_name
+    : return max_page_tag: max number of pages for thread
+    """
+    
+    #Setting parameters and getting soup for our thread
+    url = "https://www.nintendolife.com/forums/nintendo-switch/" + thread_name
+    r = requests.get(url) 
+    html_text = r.text
+    soup = BeautifulSoup(html_text, "html.parser")
+
+    #try-except block for if a thread only has one page
+    try:
+        pagination_tags = soup.findAll("a", {"class": "accent-border accent-bg-hover accent"}) #just pagination tags
+        page_tag_numbers = []
+        for tag in pagination_tags:
+            page_tag_numbers.append(int(tag.contents[0]))
+        max_page_tag = max(page_tag_numbers)
+        return max_page_tag
+    
+    except:
+        max_page_tag = 1
+        return max_page_tag
+    
+#Function for getting all of the users from a thread
+def get_users(thread_name, max_pages):
+    '''Function for collecting all users from a thread. 
+    Params: thread name (string), max number of pages
+    returns :De-duplicated list of users'''
+    
+    users = []
+    for i in range(max_pages):
+        n_posts = i * 20
+        if n_posts == 0:            
+            #Setting parameters 
+            url = "https://www.nintendolife.com/forums/nintendo-switch/" + thread_name
+            r = requests.get(url) 
+            html_text = r.text
+            soup = BeautifulSoup(html_text, "html.parser")
+            
+            #Getting user tags
+            user_names = soup.findAll("a", {"class": "accent username"})
+            for user in user_names:
+                users.append(user.contents[0])
+
+        else:           
+            #Setting parameters 
+            url = "https://www.nintendolife.com/forums/nintendo-switch/" + thread_name + "?start=" + str(n_posts)
+            r = requests.get(url) 
+            html_text = r.text
+            soup = BeautifulSoup(html_text, "html.parser")
+            
+            #Getting user tags
+            user_names = soup.findAll("a", {"class": "accent username"})
+            for user in user_names:
+                users.append(user.contents[0])
+                
+    #Deduplicating List
+    deduped_users = list(set(users))
+    return deduped_users
--- Nintendo_Project/Nintendo/src/main.py
+++ Nintendo_Project/Nintendo/src/main.py
@@ -1,12 +1,23 @@
 import os
 import sys
+import pandas as pd
 
 sys.path.append(os.getcwd())
 
 import Nintendo.src.NintendoLife_Scraping as Nintendo
 
-link3710_pages = Nintendo.get_max_pages("link3710")
-link3710_ratings = Nintendo.games_ratings_to_df("link3710", link3710_pages)
+thread_pages = Nintendo.get_max_pages_thread("the_switch_eshop_recommendations")
+thread_users = Nintendo.get_users("the_switch_eshop_recommendations", thread_pages)
 
-Quarth_pages = Nintendo.get_max_pages("Quarth")
-Quarth_ratings = Nintendo.games_ratings_to_df("Quarth", Quarth_pages)   
+df = pd.DataFrame()
+
+for user in thread_users:
+    max_pages = Nintendo.get_max_pages(user)
+    user_games_ratings = Nintendo.games_ratings_to_df(user, max_pages)
+    df = df.append(user_games_ratings)
+
+#link3710_pages = Nintendo.get_max_pages("link3710")
+#link3710_ratings = Nintendo.games_ratings_to_df("link3710", link3710_pages)
+
+#Quarth_pages = Nintendo.get_max_pages("Quarth")
+#Quarth_ratings = Nintendo.games_ratings_to_df("Quarth", Quarth_pages)   
